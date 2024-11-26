c5bdb72a15096c1adae3f32e6feb6ffb65ab7a00
Daniel Gomez <165719690+Daniel-g22@users.noreply.github.com>
2024-11-26 01:49:27 -0800

    Create Ratings.cs

---
+++ dotnet/Ratings.cs
@@ -0,0 +1,15 @@
+using Sabio.Models.Domain.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Domain.ProviderRating
+{
+    public class Ratings
+    {
+        public int Rating { get; set; }
+        public int ConsumerId { get; set; }
+    }
+}
